b9146d61436e484f6290ed4ed102a8a47710db48
Josh <ahimaz.joshua@gmail.com>
2022-06-09 13:50:20 +0800

    Removed UserData

--- src/util/myData.ts
+++ src/util/myData.ts
@@ -1,7 +1,7 @@
 export const userData = {
-    "kusama-address" : "DmZvhEFwbhaxQCguYwKTxZrXLpLT16jjtDWMh2zVhxnwFzp",
-    "extra-link" : "https://joshuaahimaz.com/",
-    "extra-link-text" : "Home",
-    "my-name" : "Joshua Ahimaz",
-    "my-social" : "https://www.linkedin.com/in/joshuaahimaz/",
+    "kusama-address" : "",
+    "extra-link" : "",
+    "extra-link-text" : "",
+    "my-name" : "",
+    "my-social" : "",
 }
